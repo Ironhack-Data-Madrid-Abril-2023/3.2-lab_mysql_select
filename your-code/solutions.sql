bb27b7ca599b174f301feb68ab69451f3e12c2f0
Carolina Rivera <riveracarolina000@gmail.com>
2023-04-25 17:36:24 +0200

    last commit

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -31,7 +31,7 @@ GROUP BY AUTHOR_ID, publishers.pub_name;
 SELECT authors.au_id as AUTHOR_ID,
  authors.au_lname as LAST_NAME,
  authors.au_fname as FIRST_NAME,
- COUNT(titles.title_id) as TOTAL
+ COUNT(sales.qty) as TOTAL
 FROM authors
 JOIN titleauthor 
 ON authors.au_id = titleauthor.au_id
@@ -39,6 +39,8 @@ JOIN titles
 ON titleauthor.title_id = titles.title_id
 JOIN publishers
 ON titles.pub_id = publishers.pub_id
+JOIN sales
+on titles.title_id = sales.title_id
 GROUP BY AUTHOR_ID
 ORDER BY TOTAL DESC LIMIT 3;
 
@@ -47,7 +49,7 @@ ORDER BY TOTAL DESC LIMIT 3;
 SELECT authors.au_id as AUTHOR_ID,
  authors.au_lname as LAST_NAME,
  authors.au_fname as FIRST_NAME,
- COUNT(titles.title_id) as TOTAL
+ count(titles.title_id) as TOTAL
 FROM authors
 LEFT JOIN titleauthor 
 ON authors.au_id = titleauthor.au_id
@@ -55,5 +57,7 @@ LEFT JOIN titles
 ON titleauthor.title_id = titles.title_id
 LEFT JOIN publishers
 ON titles.pub_id = publishers.pub_id
+LEFT JOIN sales
+on titles.title_id = sales.title_id
 GROUP BY AUTHOR_ID
 ORDER BY TOTAL DESC;

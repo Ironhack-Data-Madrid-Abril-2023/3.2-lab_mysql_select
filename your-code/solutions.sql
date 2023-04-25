d1a235d0b077305a6dbd5a5cbf93446d8965977b
Carlos Palomares <carlospalomaresgarcia@gmail.com>
2023-04-25 18:27:42 +0200

    [Carlos Palomares] (3.2-lab_mysql_select)

---
+++ your-code/solutions.sql
@@ -0,0 +1,42 @@
+/* Challenge 1*/
+
+SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME' , titles.title AS 'TITLE', publishers.pub_name AS 'PUBLISHER'
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+LEFT JOIN publishers ON titles.pub_id = publishers.pub_id; 
+
+/* Challenge 2*/
+
+SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME', publishers.pub_name AS 'PUBLISHER', COUNT(*) AS 'TITLE COUNT'
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+LEFT JOIN publishers ON titles.pub_id = publishers.pub_id
+GROUP BY authors.au_id, authors.au_lname, publishers.pub_name
+ORDER BY authors.au_id DESC;
+
+/* Challenge 3*/
+
+SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME', MAX(sales.qty) AS 'TOTAL'
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+LEFT JOIN sales ON titles.title_id = sales.title_id
+GROUP BY authors.au_id, authors.au_lname, sales.qty
+ORDER BY sales.qty DESC
+LIMIT 3;
+
+/* Challenge 4*/
+
+SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME', IFNULL(MAX(sales.qty),0) AS 'TOTAL'
+FROM authors
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+LEFT JOIN sales ON titles.title_id = sales.title_id
+GROUP BY authors.au_id, authors.au_lname, sales.qty
+ORDER BY TOTAL DESC
+LIMIT 23;
+
+
+

9e122e9f3d7292fd5fd68824a6b80c8ba5855132
David Aparicio <apariciofernandez.d@gmail.com>
2023-04-25 18:10:27 +0200

    3.2-lab_mysql_select_CHALLENGE2

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,6 +1,6 @@
 Challenge 1.
 
-SELECT authors.au_id AS 'AUTHORS_ID',
+SELECT authors.au_id AS 'AUTHOR_ID',
 		authors.au_lname AS 'LAST NAME',
         authors.au_fname AS 'FIRST NAME',
         titles.title AS 'TITLE',
@@ -9,5 +9,22 @@ FROM authors
 		LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
 		LEFT JOIN titles ON titleauthor.title_id = titles.title_id
 		LEFT JOIN publishers ON titles.pub_id = publishers.pub_id
+        ;
+
+
+
+Challenge 2.
+
+SELECT authors.au_id AS 'AUTHOR_ID',
+		authors.au_lname AS 'LAST NAME',
+        authors.au_fname AS 'FIRST NAME',
+        publishers.pub_name AS 'PUBLISHER',
+        COUNT(titles.title_id) AS 'TITLE COUNT'
+FROM authors
+		LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+		LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+		LEFT JOIN publishers ON titles.pub_id = publishers.pub_id
+GROUP BY
+		authors.au_id, publishers.pub_id
         ;
 

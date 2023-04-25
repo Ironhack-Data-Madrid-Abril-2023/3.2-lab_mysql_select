a67b7a961cb20de19aaa6145554af218e3a031ea
David Aparicio <apariciofernandez.d@gmail.com>
2023-04-25 18:40:06 +0200

    3.2-lab_mysql_select_CHALLENGE3

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -28,3 +28,22 @@ GROUP BY
 		authors.au_id, publishers.pub_id
         ;
 
+
+
+Challenge 3.
+
+SELECT authors.au_id AS 'AUTHOR_ID',
+		authors.au_lname AS 'LAST NAME',
+        authors.au_fname AS 'FIRST NAME',
+        SUM(sales.qty) AS 'TOTAL'
+FROM authors
+		authors
+        INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+		INNER JOIN titles ON titleauthor.title_id = titles.title_id
+		INNER JOIN sales ON titles.title_id = sales.title_id
+GROUP BY
+		authors.au_id
+LIMIT 3
+;
+
+

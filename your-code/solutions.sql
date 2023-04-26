e16c891ef0b13700491452220c0b75c2435bda03
David Aparicio <apariciofernandez.d@gmail.com>
2023-04-26 08:56:37 +0200

    3.2-lab_mysql_select_CHALLENGE4

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -37,13 +37,30 @@ SELECT authors.au_id AS 'AUTHOR_ID',
         authors.au_fname AS 'FIRST NAME',
         SUM(sales.qty) AS 'TOTAL'
 FROM authors
-		authors
         INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
 		INNER JOIN titles ON titleauthor.title_id = titles.title_id
 		INNER JOIN sales ON titles.title_id = sales.title_id
 GROUP BY
 		authors.au_id
+ORDER BY
+    SUM(sales.qty) DESC
 LIMIT 3
 ;
 
 
+
+Challenge 4. # no me salen los 23 autores que deberia tener el ejercicio
+
+SELECT authors.au_id AS 'AUTHOR_ID',
+		authors.au_lname AS 'LAST NAME',
+        authors.au_fname AS 'FIRST NAME',
+        SUM(sales.qty) AS 'TOTAL'
+FROM authors
+        INNER JOIN titleauthor ON authors.au_id = titleauthor.au_id
+		INNER JOIN titles ON titleauthor.title_id = titles.title_id
+		INNER JOIN sales ON titles.title_id = sales.title_id
+GROUP BY
+		authors.au_id
+ORDER BY
+		TOTAL DESC
+;
